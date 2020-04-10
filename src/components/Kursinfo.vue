b30bb61db5c3b187439756a927148972677318d9
Neda Jahadi <neda_jahadi@yahoo.com>
2020-04-10 10:18:57 +0200

    komponenter har skapat och listan av dagliga schemat har lagt till

---
+++ src/components/Kursinfo.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="info-container">
+      <div class="header-info">
+          <div>Date</div><div>Title</div><div>Content</div>
+      </div>
+      <kursinfoRad v-for="info in kursinfo" :key="info.id" :info="info" @handle-delete="deleteRow"/>
+
+  </div>
+</template>
+
+<script>
+import kursinfoRad from './KursinfoRad.vue'
+export default {
+    data: ()=> ({
+        kursinfo: [{id:1,date:'date1',title:'title1',content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'},
+                   {id:2,date:'date2',title:'title2',content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'},
+                   {id:3,date:'date3',title:'titel3',content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'},
+                   {id:4,date:'date4',title:'title4',content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'},
+                   {id:5,date:'date5',title:'title5',content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'}
+                   ]
+    }),
+    components: {
+        kursinfoRad
+    },
+    methods: {
+        deleteRow(value) {
+            this.kursinfo=this.kursinfo.filter(item=>item.id!==value);
+
+        }
+    }
+
+}
+</script>
+
+<style>
+.header-info {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    background-color: #D33A3A;
+    padding: 1em;
+}
+
+  .info-container {
+      border: 1px solid gray;
+      background-color: #C4C4C4;
+      color: black;
+
+  }
+  /*.info-container >div:nth-child(even){
+      background-color: azure;
+  }*/
+</style>
---
+++ src/components/KursinfoRad.vue
@@ -0,0 +1,65 @@
+<template>
+ <div class="kursinfo-container">
+   <div class="kursinfo" @click="clickableDiv">
+      <div>{{info.date}}</div>
+      <div>{{info.title}}</div>
+      <div>{{info.content.substring(0,20)}}...</div>
+   </div>
+   <div class="content" v-show="contentShow">
+       <div>{{info.content}}</div>
+       <div>
+         <button @click="clickData">Delete!</button>
+       </div>
+    </div>
+
+ </div>
+</template>
+
+<script>
+export default {
+  props: {
+      info: Object(null),
+  },
+  data:()=>({
+    contentShow: false
+  }),
+  methods: {
+      clickData() {
+          this.$emit('handle-delete', this.info.id);
+      },
+      clickableDiv() {
+          console.log('Div is clicked');
+          this.contentShow=!this.contentShow;
+      }
+  }
+}
+</script>
+
+<style>
+ .content {
+     border: 1px solid gray;
+     background-color: #979797;
+     color: black;
+     padding: 1em;
+ }
+
+  button {
+      width: 20%;
+    padding: 0.5em;
+    background-color: #D33A3A;
+    margin-top: 1em;
+}
+
+  .kursinfo {
+      border: solid 1px gray;
+      padding: 1em;
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+
+  }
+
+  .kursinfo:hover {
+      background-color:#737373;
+      cursor: pointer;
+  }
+</style>
